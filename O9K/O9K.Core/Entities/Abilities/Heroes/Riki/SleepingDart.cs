4a9d455c617767adb4345df92c6d5848e7f7bb89
Data-Wan <ustimenko.work@gmail.com>
2021-07-28 00:03:25 +0300

    add SleepingDart.cs to riki

---
+++ O9K/O9K.Core/Entities/Abilities/Heroes/Riki/SleepingDart.cs
@@ -0,0 +1,25 @@
+﻿namespace O9K.Core.Entities.Abilities.Heroes.Riki
+{
+    using Base;
+    using Base.Types;
+
+    using Divine.Entity.Entities.Abilities;
+    using Divine.Entity.Entities.Abilities.Components;
+    using Divine.Entity.Entities.Units.Components;
+
+    using Helpers;
+
+    using Metadata;
+
+    [AbilityId(AbilityId.riki_poison_dart)]
+    public class SleepingDart : RangedAbility, IDisable, INuke
+    {
+
+        public SleepingDart(Ability baseAbility)
+            : base(baseAbility)
+        {
+        }
+
+        public UnitState AppliesUnitState { get; } = UnitState.Stunned;
+    }
+}
