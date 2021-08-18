685da426f554b124c9a2e5d8ca25d602850628c6
Data-Wan <ustimenko.work@gmail.com>
2021-08-18 23:48:31 +0300

    add DefenseMatrixBase for evader.

---
+++ O9K/O9K.Evader/Abilities/Heroes/Tinker/DefenseMatrix/DefenseMatrixBase.cs
@@ -0,0 +1,24 @@
+﻿namespace O9K.Evader.Abilities.Heroes.Tinker.DefenseMatrix
+{
+    using Base;
+    using Base.Usable.CounterAbility;
+
+    using Core.Entities.Abilities.Base;
+    using Core.Entities.Metadata;
+
+    using Divine.Entity.Entities.Abilities.Components;
+
+    [AbilityId(AbilityId.tinker_defense_matrix)]
+    internal class DefenseMatrixBase : EvaderBaseAbility, IUsable<CounterAbility>
+    {
+        public DefenseMatrixBase(Ability9 ability)
+            : base(ability)
+        {
+        }
+
+        public CounterAbility GetUsableAbility()
+        {
+            return new DefenseMatrixUsable(this.Ability, this.ActionManager, this.Menu);
+        }
+    }
+}
