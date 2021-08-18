3b9d28742e8f3d827d1768c654b32db2e5175e86
Data-Wan <ustimenko.work@gmail.com>
2021-08-18 17:41:28 +0300

    add fix for riki

---
+++ O9K/O9K.AIO/Heroes/Riki/Abilities/RikiBlinkStrike.cs
@@ -0,0 +1,46 @@
+﻿namespace O9K.AIO.Heroes.Riki.Abilities
+{
+    using AIO.Abilities;
+
+    using Core.Entities.Abilities.Base;
+    using Core.Helpers;
+
+    using TargetManager;
+
+    internal class RikiBlinkStrike : BlinkAbility
+    {
+        public RikiBlinkStrike(ActiveAbility ability)
+            : base(ability)
+        {
+        }
+
+        public override bool UseAbility(TargetManager targetManager, Sleeper comboSleeper, bool aoe)
+        {
+            var target = targetManager.Target;
+            var attackRange = this.Owner.GetAttackRange(target);
+            var distance = target.Distance(this.Owner);
+
+            if (distance <= attackRange + 100)
+            {
+                return false;
+            }
+
+            if (distance <= attackRange + 250 && this.Owner.Speed > targetManager.Target.Speed + 50)
+            {
+                return false;
+            }
+
+            if (this.Ability.UseAbility(target))
+            {
+                return  false;
+            }
+
+            var delay = this.Ability.GetCastDelay(target);
+            comboSleeper.Sleep(delay);
+            this.Sleeper.Sleep(delay + 0.5f);
+            this.OrbwalkSleeper.Sleep(delay);
+
+            return true;
+        }
+    }
+}
