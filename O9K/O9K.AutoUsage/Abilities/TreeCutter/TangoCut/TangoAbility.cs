d65a8ee686b90198b64a4416abfd19313898680d
Data-Wan <ustimenko.work@gmail.com>
2021-08-30 21:59:49 +0300

    add TreeCutAbilities for AutoUsage.

---
+++ O9K/O9K.AutoUsage/Abilities/TreeCutter/TangoCut/TangoAbility.cs
@@ -0,0 +1,19 @@
+﻿namespace O9K.AutoUsage.Abilities.TreeCutter.TangoCut
+{
+    using Core.Entities.Abilities.Base.Components.Base;
+    using Core.Entities.Metadata;
+
+    using Divine.Entity.Entities.Abilities.Components;
+
+    using Settings;
+
+    [AbilityId(AbilityId.item_tango)]
+    [AbilityId(AbilityId.item_tango_single)]
+    internal class TangoAbility : TreeCutAbility
+    {
+        public TangoAbility(IActiveAbility ability, GroupSettings settings) : base(ability)
+        {
+            this.settings = new TreeCutSettings(settings.Menu, ability);
+        }
+    }
+}
