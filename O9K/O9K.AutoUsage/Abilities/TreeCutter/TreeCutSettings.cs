a982b54786dd087aa20f4c0b44ae3a2d86459aa1
Data-Wan <ustimenko.work@gmail.com>
2021-09-03 18:34:47 +0300

    add settings for Activation after X minutes TreeCutAbility

--- O9K/O9K.AutoUsage/Abilities/TreeCutter/TreeCutSettings.cs
+++ O9K/O9K.AutoUsage/Abilities/TreeCutter/TreeCutSettings.cs
@@ -7,14 +7,19 @@
     {
         public MenuSlider DelayBeforeNextActivation;
 
+        public MenuSlider ActiveAfterXMinutes;
+
         public TreeCutSettings(Menu settings, IActiveAbility ability)
         {
             var menu = settings.GetOrAdd(new Menu(ability.DisplayName, ability.Name).SetTexture(ability.Name));
 
             this.DelayBeforeNextActivation =
                 menu.Add(new MenuSlider("Time (in ms) before next activation (GLOBAL! WORK ON ALL CUTTERS)", 3000, 0,
-                    5000));
-        }
+                                        5000));
 
+            this.ActiveAfterXMinutes =
+                menu.Add(new MenuSlider("Use abilities after X minutes", 10, 0,
+                                        100));
+        }
     }
 }
