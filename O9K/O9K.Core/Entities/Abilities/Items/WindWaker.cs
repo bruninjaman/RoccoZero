488af1aaf214d7af81debdcbbfaddbe02c48c895
RoccoZero <osmanbonus007@hotmail.com>
2021-09-06 14:11:04 +0300

    test C# 10

--- O9K/O9K.Core/Entities/Abilities/Items/WindWaker.cs
+++ O9K/O9K.Core/Entities/Abilities/Items/WindWaker.cs
@@ -1,34 +1,33 @@
-﻿namespace O9K.Core.Entities.Abilities.Items
+﻿namespace O9K.Core.Entities.Abilities.Items;
+
+using Base;
+using Base.Components;
+using Base.Types;
+
+using Divine.Entity.Entities.Abilities;
+using Divine.Entity.Entities.Abilities.Components;
+using Divine.Entity.Entities.Units.Components;
+
+using Helpers;
+
+using Metadata;
+
+[AbilityId(AbilityId.item_wind_waker)]
+public class WindWaker : RangedAbility, IDisable, IShield, IAppliesImmobility
 {
-    using Base;
-    using Base.Components;
-    using Base.Types;
-
-    using Divine.Entity.Entities.Abilities;
-    using Divine.Entity.Entities.Abilities.Components;
-    using Divine.Entity.Entities.Units.Components;
-
-    using Helpers;
-
-    using Metadata;
-
-    [AbilityId(AbilityId.item_wind_waker)]
-    public class WindWaker : RangedAbility, IDisable, IShield, IAppliesImmobility
+    public WindWaker(Ability baseAbility)
+        : base(baseAbility)
     {
-        public WindWaker(Ability baseAbility)
-            : base(baseAbility)
-        {
-            this.DurationData = new SpecialData(baseAbility, "cyclone_duration");
-        }
-
-        public string ImmobilityModifierName { get; } = "modifier_wind_waker";
-
-        public UnitState AppliesUnitState { get; } = UnitState.Invulnerable;
-
-        public string ShieldModifierName { get; } = "modifier_wind_waker";
-
-        public bool ShieldsAlly { get; } = true;
-
-        public bool ShieldsOwner { get; } = true;
+        this.DurationData = new SpecialData(baseAbility, "cyclone_duration");
     }
+
+    public string ImmobilityModifierName { get; } = "modifier_wind_waker";
+
+    public UnitState AppliesUnitState { get; } = UnitState.Invulnerable;
+
+    public string ShieldModifierName { get; } = "modifier_wind_waker";
+
+    public bool ShieldsAlly { get; } = true;
+
+    public bool ShieldsOwner { get; } = true;
 }
