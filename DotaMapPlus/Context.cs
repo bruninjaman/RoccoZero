8d5f4d13506d3070ee546070128f7e16b2d9aac5
RoccoZero <RoccoZero@outlook.com>
2021-10-23 10:09:52 +0300

    DotaMapPlus removed ZoomHack, use Plugin Camera

--- DotaMapPlus/Context.cs
+++ DotaMapPlus/Context.cs
@@ -5,7 +5,7 @@ using Divine.Numerics;
 
 internal sealed class Context
 {
-    private readonly ZoomHack ZoomHack;
+    //private readonly ZoomHack ZoomHack;
 
     private readonly ConsoleCommands ConsoleCommands;
 
@@ -16,7 +16,7 @@ internal sealed class Context
         var rootMenu = MenuManager.CreateRootMenu("DotaMapPlus");
         rootMenu.SetFontColor(Color.Aqua);
 
-        ZoomHack = new ZoomHack(rootMenu);
+        //ZoomHack = new ZoomHack(rootMenu);
 
         ConsoleCommands = new ConsoleCommands(rootMenu);
 
@@ -25,7 +25,7 @@ internal sealed class Context
 
     public void Dispose()
     {
-        ZoomHack.Dispose();
+        //ZoomHack.Dispose();
         ConsoleCommands.Dispose();
         WeatherHack.Dispose();
     }
