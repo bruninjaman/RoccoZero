886bd0cd948b5d79ec12d78468a942907d4a4951
RoccoZero <RoccoZero@outlook.com>
2022-03-15 07:03:41 +0300

    added zeus new ability

---
+++ O9K/O9K.Core/Entities/Abilities/Heroes/Zeus/HeavenlyJump.cs
@@ -0,0 +1,58 @@
+﻿namespace O9K.Core.Entities.Abilities.Heroes.Zeus;
+
+using System.Linq;
+
+using Base;
+using Base.Types;
+
+using Divine.Entity.Entities.Abilities;
+using Divine.Entity.Entities.Abilities.Components;
+
+using Entities.Units;
+
+using Helpers;
+using Helpers.Damage;
+
+using Managers.Entity;
+
+using Metadata;
+
+[AbilityId(AbilityId.zuus_heavenly_jump)]
+public class HeavenlyJump : ActiveAbility, INuke
+{
+    private StaticField staticField;
+
+    public HeavenlyJump(Ability baseAbility)
+        : base(baseAbility)
+    {
+        this.DamageData = new SpecialData(baseAbility, "damage");
+        this.RadiusData = new SpecialData(baseAbility, "range");
+    }
+
+    public override Damage GetRawDamage(Unit9 unit, float? remainingHealth = null)
+    {
+        var damage = base.GetRawDamage(unit, remainingHealth);
+
+        if (this.staticField?.CanBeCasted() == true)
+        {
+            damage += this.staticField.GetRawDamage(unit, remainingHealth);
+        }
+
+        return damage;
+    }
+
+    internal override void SetOwner(Unit9 owner)
+    {
+        base.SetOwner(owner);
+
+        var ability = EntityManager9.BaseAbilities.FirstOrDefault(
+            x => x.Id == AbilityId.zuus_static_field && x.Owner?.Handle == owner.Handle);
+
+        if (ability == null)
+        {
+            return;
+        }
+
+        this.staticField = (StaticField)EntityManager9.AddAbility(ability);
+    }
+}
