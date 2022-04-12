9fca164ee9cfe85c6daeaa779afcfb2509351af0
RoccoZero <RoccoZero@outlook.com>
2022-04-12 22:25:38 +0300

    fixed Morphling Waveform

--- O9K/O9K.Core/Entities/Abilities/Heroes/Morphling/Waveform.cs
+++ O9K/O9K.Core/Entities/Abilities/Heroes/Morphling/Waveform.cs
@@ -13,8 +13,6 @@ using Metadata;
 [AbilityId(AbilityId.morphling_waveform)]
 public class Waveform : LineAbility, INuke, IBlink
 {
-    private readonly SpecialData castRangeData;
-
     private bool talentLearned;
 
     public Waveform(Ability baseAbility)
@@ -22,7 +20,6 @@ public class Waveform : LineAbility, INuke, IBlink
     {
         this.RadiusData = new SpecialData(baseAbility, "width");
         this.SpeedData = new SpecialData(baseAbility, "speed");
-        this.castRangeData = new SpecialData(baseAbility, "abilitycastrange");
     }
 
     public BlinkType BlinkType { get; } = BlinkType.Blink;
@@ -37,14 +34,6 @@ public class Waveform : LineAbility, INuke, IBlink
             }
 
             return this.talentLearned = this.Owner.GetAbilityById(AbilityId.special_bonus_unique_morphling_6)?.Level > 0;
-        }
-    }
-
-    protected override float BaseCastRange
-    {
-        get
-        {
-            return this.castRangeData.GetValue(this.Level);
         }
     }
 }
