f214d6ba85b654215c511f84bf46ddf6dcce5f21
JumpAttacker <JumpAttacker@gmail.com>
2022-04-16 22:48:16 +0300

    Added phantom assassin aghanim's blur

--- O9K/O9K.Core/Entities/Abilities/Heroes/PhantomAssassin/Blur.cs
+++ O9K/O9K.Core/Entities/Abilities/Heroes/PhantomAssassin/Blur.cs
@@ -1,10 +1,8 @@
 ﻿namespace O9K.Core.Entities.Abilities.Heroes.PhantomAssassin;
 
 using Base;
-
 using Divine.Entity.Entities.Abilities;
 using Divine.Entity.Entities.Abilities.Components;
-
 using Metadata;
 
 [AbilityId(AbilityId.phantom_assassin_blur)]
@@ -13,5 +11,18 @@ public class Blur : ActiveAbility
     public Blur(Ability baseAbility)
         : base(baseAbility)
     {
+    }
+
+    public override float CastPoint
+    {
+        get
+        {
+            if (Owner.HasAghanimsScepter)
+            {
+                return 0;
+            }
+
+            return base.CastPoint;
+        }
     }
 }
