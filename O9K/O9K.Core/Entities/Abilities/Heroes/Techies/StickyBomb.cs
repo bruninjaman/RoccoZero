1e192c865f49819639eea2df3f565412862c842a
kolay <kolay.va@gmail.com>
2022-06-26 01:21:08 +0300

    fixed techies

--- O9K/O9K.Core/Entities/Abilities/Heroes/Techies/StickyBomb.cs
+++ O9K/O9K.Core/Entities/Abilities/Heroes/Techies/StickyBomb.cs
@@ -6,7 +6,10 @@ using Divine.Entity.Entities.Abilities.Components;
 using O9K.Core.Entities.Abilities.Base;
 using O9K.Core.Entities.Abilities.Base.Types;
 using O9K.Core.Entities.Metadata;
+using O9K.Core.Entities.Units;
 using O9K.Core.Helpers;
+using O9K.Core.Prediction.Data;
+using System.Collections.Generic;
 
 [AbilityId(AbilityId.techies_sticky_bomb)]
 public class StickyBomb : CircleAbility, INuke, IDebuff
@@ -20,4 +23,29 @@ public class StickyBomb : CircleAbility, INuke, IDebuff
     }
 
     public string DebuffModifierName { get; } = "modifier_techies_sticky_bomb_slow";
+
+    public virtual PredictionInput9 GetPredictionInput(Unit9 target, List<Unit9> aoeTargets = null)
+    {
+        var input = new PredictionInput9
+        {
+            Caster = this.Owner,
+            Target = target,
+            CollisionTypes = this.CollisionTypes,
+            Delay = this.CastPoint + this.ActivationDelay + InputLag + 1.2f,
+            Speed = float.MaxValue,
+            Range = this.Range,
+            Radius = this.Radius,
+            CastRange = this.CastRange,
+            SkillShotType = this.SkillShotType,
+            RequiresToTurn = !this.NoTargetCast
+        };
+
+        if (aoeTargets != null)
+        {
+            input.AreaOfEffect = this.HasAreaOfEffect;
+            input.AreaOfEffectTargets = aoeTargets;
+        }
+
+        return input;
+    }
 }
